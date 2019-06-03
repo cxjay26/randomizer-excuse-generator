3a1aee6e6fffab0da7190ae4f5236f7445f4232b
cxjay26 <christianv042393@gmail.com>
2019-06-03 23:09:34 +0000

    first commit

--- src/js/index.js
+++ src/js/index.js
@@ -6,6 +6,24 @@ import "../assets/img/4geeks.ico";
 //import 'breathecode-dom'; //DOM override to make JS easier to use
 import "../style/index.scss";
 
-window.onload = function() {
-  console.log("Hello Rigo from the console!");
-};
+const who = ["the dog", "my granma", "his turtle", "my bird"];
+const what = ["eat", "pissed", "crushed", "broked"];
+const when = [
+  "before the class",
+  "right in time",
+  "when I finished",
+  "during my lunch",
+  "while I was praying"
+];
+
+function change() {
+  elem.innerHTML =
+    who[Math.floor(Math.random() * 4)] +
+    " " +
+    what[Math.floor(Math.random() * 4)] +
+    " " +
+    when[Math.floor(Math.random() * 5)];
+}
+
+let elem = document.getElementById("excuses");
+setInterval(change, 1000);
